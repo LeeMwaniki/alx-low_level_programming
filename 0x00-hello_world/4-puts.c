39ce0007bf9e761b73754b24e0c20e61a41bd052
LeeMwaniki <theleembugua@gmail.com>
2022-07-06 04:21:39 +0300

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *
+ *Return: 0 after printing output
+ */
+int main(void)
+{
+	puts("\"Programming is like biulding a multilingual puzzle");
+	return (0);
+}
