5f1c97bfe869e1b3c9a02511a479b3fcf9342118
LeeMwaniki <theleembugua@gmail.com>
2022-07-13 23:02:54 +0300

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i = '0';
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}
